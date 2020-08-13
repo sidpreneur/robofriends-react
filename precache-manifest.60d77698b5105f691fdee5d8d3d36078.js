ed823f197da897b095c7f5c92a0583c8076e5155
sidpreneur <sidpreneur@gmail.com>
2020-08-13 18:12:54 +0530

    Updates

---
+++ precache-manifest.60d77698b5105f691fdee5d8d3d36078.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1780a7ccf4933f6457182ac2310fb2ba",
+    "url": "/robofriends-react/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/robofriends-react/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "fa83647038c1e50f21de",
+    "url": "/robofriends-react/static/css/main.83e575fa.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/robofriends-react/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends-react/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa83647038c1e50f21de",
+    "url": "/robofriends-react/static/js/main.b9a849df.chunk.js"
+  },
+  {
+    "revision": "4a26307f729ee557879e",
+    "url": "/robofriends-react/static/js/runtime-main.b0d87439.js"
+  }
+]);
